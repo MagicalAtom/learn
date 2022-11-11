1f736fad34a5e2c4e3e6bf60554cbcdc00345900
MagicalAtom <magicsoftware.org@gmail.com>
2022-11-11 07:06:36 -0800

    add Register Form

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,17 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('user_name')->nullable();
             $table->string('email')->unique();
+            $table->string('phone_number')->nullable();
+            $table->string('headline')->nullable();
+            $table->text('biography')->nullable();
+            $table->string('website')->nullable();
+            $table->string('linkedin')->nullable();
+            $table->string('instagram')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('telegram')->nullable();
+            $table->string('twitter')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,59 @@
+@extends('auth.layout.master')
+
+
+@section('title','صفحه ثبت نام')
+
+
+@section('content')
+<form action="{{ route('register') }}" class="form" method="POST">
+    @csrf
+            <a class="account-logo" href="index.html">
+                <img src="img/weblogo.png" alt="">
+            </a>
+            <div class="form-content form-account">
+                <input type="text" class="txt  @error('name') is-invalid @enderror" placeholder="نام و نام خانوادگی"
+                       name="name" value="{{ old('name') }}" required autocomplete="name" autofocus
+                >
+                @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+
+                <input type="text" class="txt txt-l  @error('email') is-invalid @enderror" placeholder="ایمیل"
+                       name="email" value="{{ old('email') }}" required autocomplete="email"
+                >
+                @error('email')
+                        <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                        @enderror
+
+                <input type="text" class="txt txt-l  @error('phone_number') is-invalid @enderror" placeholder="تلفن همراه"
+                       name="phone_number" value="{{ old('phone_number') }}"  autocomplete="phone_number"
+                >
+                @error('phone_number')
+                <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                @enderror
+                <input type="text" class="txt txt-l @error('password') is-invalid @enderror" placeholder="رمز عبور" name="password" required autocomplete="new-password">
+                @error('password')
+                        <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                        @enderror
+                <input id="password-confirm" type="password" placeholder="تکرار رمز عبور" class="txt txt-1" name="password_confirmation" required autocomplete="new-password">
+
+                <span class="rules">رمز عبور باید حداقل ۶ کاراکتر و ترکیبی از حروف بزرگ، حروف کوچک، اعداد و کاراکترهای غیر الفبا مانند !@#$%^&*() باشد.</span>
+                <br>
+                <button class="btn continue-btn">ثبت نام و ادامه</button>
+
+            </div>
+            <div class="form-footer">
+                <a href="login.html">صفحه ورود</a>
+            </div>
+        </form>
+@endsection
+
+
